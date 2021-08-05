24427c5aadb6d6c7ea4401f756c1c3eb9278c34c
gufranyes <gufran.yesilyurt@ozu.edu.tr>
2021-08-05 20:59:31 +0300

    Using Dynamic Delegates

--- Source/ToonTanks/Actors/ProjectileBase.cpp
+++ Source/ToonTanks/Actors/ProjectileBase.cpp
@@ -4,6 +4,7 @@
 #include "ProjectileBase.h"
 #include "Components/StaticMeshComponent.h"
 #include "GameFramework/ProjectileMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 AProjectileBase::AProjectileBase()
@@ -17,14 +18,29 @@ AProjectileBase::AProjectileBase()
 	ProjectileMovement -> InitialSpeed = MovementSpeed;
 	ProjectileMovement -> MaxSpeed = MovementSpeed;
 	InitialLifeSpan = 3.0f;
-	
+
 }
 
 // Called when the game starts or when spawned
 void AProjectileBase::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	ProjectileMesh -> OnComponentHit.AddDynamic(this, &AProjectileBase::OnHit);
 }
 
 
+
+void AProjectileBase::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit) 
+{
+	AActor* MyOwner = GetOwner();
+	if (!MyOwner)
+	{
+		return;
+	}
+	if (OtherActor && OtherActor != this && OtherActor != MyOwner)
+	{
+		UGameplayStatics::ApplyDamage(OtherActor, Damage, MyOwner -> GetInstigatorController(), this, DamageType);
+	}
+	
+	Destroy();
+}
